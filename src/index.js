64caf92baf1ff8db3413438a3ddbfa724cb43b85
footballjones <footballjones308@gmail.com>
2023-04-12 18:05:26 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("DOM loaded.")
 });
+
+function addToList(text) {
+  const li = document.createElement("li")
+  li.className = "listElement"
+  li.textContent = text
+
+  document.getElementById("tasks").appendChild(li)
+}
+
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => handleTask(e))
+
+
+// 
+function handleTask(e) {
+  e.preventDefault()
+  const input = document.querySelector("input").value
+  addToList(input)
+}
